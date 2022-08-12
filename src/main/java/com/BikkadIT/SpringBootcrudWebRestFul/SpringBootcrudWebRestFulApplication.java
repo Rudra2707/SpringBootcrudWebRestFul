5d8bfbb09188b8d28c1fd16cc191a1a3f5960b47
Rudra2707 <ushirjitendra1305@gmail.com>
2022-08-12 09:14:10 +0530

    Project added to Central repo

---
+++ src/main/java/com/BikkadIT/SpringBootcrudWebRestFul/SpringBootcrudWebRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SpringBootcrudWebRestFul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcrudWebRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcrudWebRestFulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/SpringBootcrudWebRestFul/SpringBootcrudWebRestFulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.SpringBootcrudWebRestFul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootcrudWebRestFulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
